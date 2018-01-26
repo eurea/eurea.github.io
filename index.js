835c22bb755765edb027fba2dc5adbba980317e7
Alexander <sashok935@gmail.com>
2018-01-26 22:39:09 +0300

    Added cookie rolls save

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function calcRolls(){
+    //console.log(Math.floor($("#crystals").val() / 300));
+    var date = new Date();
+    date = new Date(date.getTime() + 1000 * 60 * 60 * 24 * 365);
+    document.cookie = 'crystals=' + $("#crystals").val() + ';expires=' + date.toGMTString() + ';';
+    document.cookie = 'singles=' + $("#singles").val() + ';expires=' + date.toGMTString() + ';';
+    document.cookie = 'tens=' + $("#tens").val() + ';expires=' + date.toGMTString() + ';';
+    console.log(document.cookie);
+    $("#total-rolls").val(Number(Math.floor($("#crystals").val() / 300)) + Number($("#singles").val()) + Number($("#tens").val() * 10));
+    $("#percentage").val(~~(($("#total-rolls").val() / 300).toFixed(2) * 100) + "%");
+}
 $(document).ready(function(){
+
+    $("#crystals").val(getCookie("crystals"));
+    $("#singles").val(getCookie("singles"));
+    $("#tens").val(getCookie("tens"));
+    calcRolls();
+
     $("#bonus-info").popover({
         content:"Usually comes from Wonders and Elemental Skills.",
         trigger:"hover",
         placement:"bottom",
     });
+
     $(".trigger").on('input click',function(){
     	var exp_table = []
     	var max_level = 150;
@@ -69,9 +103,6 @@ $(document).ready(function(){
             $("#error-content").html("");
         }
     });
-    $(".trigger-crystal").on('input click',function(){
-        console.log(Math.floor($("#crystals").val() / 300));
-        $("#total-rolls").val(Number(Math.floor($("#crystals").val() / 300)) + Number($("#singles").val()) + Number($("#tens").val() * 10));
-        $("#percentage").val(~~(($("#total-rolls").val() / 300).toFixed(2) * 100) + "%");
-    });
+    
+    $(".trigger-crystal").on('input click', calcRolls());
 });
