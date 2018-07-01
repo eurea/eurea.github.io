8f376bdbfa720586e74554e20b1aa73c40237cfb
Alexander <sashok935@gmail.com>
2018-07-01 13:55:51 +0300

    remove old code, fix 100% on 299 rolls

--- js/script.js
+++ js/script.js
@@ -23,31 +23,7 @@ function calcRolls(){
     document.cookie = 'tens=' + $("#tens").val() + ';expires=' + date.toGMTString() + ';';
     console.log(document.cookie);
     $("#total-rolls").val(Number(Math.floor($("#crystals").val() / 300)) + Number($("#singles").val()) + Number($("#tens").val() * 10));
-    $("#percentage").val(~~(($("#total-rolls").val() / 300).toFixed(2) * 100) + "%");
-}
-
-function incrValueSSR() {
-    var value = $("#ssr").val();
-    value = isNaN(value) ? 0 : value;
-    $("#ssr").val(++value);
-}
-
-function decrValueSSR() {
-    var value = $("#ssr").val();
-    value = isNaN(value) ? 0 : value;
-    $("#ssr").val(--value); 
-}
-
-function incrValueMoons() {
-    var value = $("#moons").val();
-    value = isNaN(value) ? 0 : value;
-    $("#moons").val(++value);
-}
-
-function decrValueMoons() {
-    var value = $("#moons").val();
-    value = isNaN(value) ? 0 : value;
-    $("#moons").val(--value); 
+    $("#percentage").val(~~(($("#total-rolls").val() / 300) * 100) + "%");
 }
 
 $(document).ready(function(){
