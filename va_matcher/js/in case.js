faaefc4d57889a4eb5dab2cf9ad8f79dc158fe37
Eurea <eureaa@gmail.com>
2018-07-27 10:27:05 +0800

    multiple page support. much bandaid

--- va_matcher/js/in case.js
+++ va_matcher/js/in case.js
@@ -34,3 +34,39 @@ $(document).ready(function(){
 });
 
 
+$(document).ready(function(){
+	$("#search").on('click', function(){
+		$('#results > tbody:last-child').html("<tr><td>Title</td><td>Character</td></tr>"); // reset table
+		var userTitles = [];
+		var pageCount = 1;
+		var lastPage = false;
+		function loopIt(){
+			$.getJSON('https://allorigins.me/get?url=' + encodeURIComponent($('#list').val() + "?c=all;v=0;t=-1;o=a;s=title;p=" + String(pageCount)) + '&callback=?').then(function(data){
+			    pageCount++;
+			    var userPage = $("<div></div>");
+			    userPage.html(data.contents);
+			    userTable = $('table[class="stripe"]', userPage);
+			    if ($('td[class="tc3_5"]', userTable) != null) { //check if there are still more entries
+					$('td[class="tc3_5"]:has(a)', userTable).each(function(i, obj){
+						userTitles.push($(this).text());
+					});
+					loopIt();
+			    } 
+			});
+		}
+		loopIt();
+
+		$.getJSON('https://allorigins.me/get?url=' + encodeURIComponent($('#target').val()) + '&callback=?', function(data){
+		    var allTables = $("<div></div>");
+		    allTables.html(data.contents);
+			charTable = $('table[class="stripe"]', allTables).last();
+			$('td[class="tc1"]:has(a)', charTable).each(function(i, obj){
+				if (userTitles.includes($(this).text())) {
+					$('#results > tbody:last-child').append("<tr><td>" + $(this).html().replace("/", "https://vndb.org/") + "</td><td>" + $('td[class="tc3"]:has(a)', charTable).eq(i).html().replace("/", "https://vndb.org/") + "</td></tr>");
+				}
+			});
+		});
+	});
+});
+
+
--- va_matcher/js/script.js
+++ va_matcher/js/script.js
@@ -2,30 +2,32 @@ $(document).ready(function(){
 	$("#search").on('click', function(){
 		$('#results > tbody:last-child').html("<tr><td>Title</td><td>Character</td></tr>"); // reset table
 		var userTitles = [];
-		var pageCount = 1;
 		var lastPage = false;
-		$.getJSON('https://allorigins.me/get?url=' + encodeURIComponent($('#list').val() + "?c=all;v=0;t=-1;o=a;s=title;p=" + String(pageCount)) + '&callback=?', function(data){
-		    console.log($('#list').val() + "?c=all;v=0;t=-1;o=a;s=title;p=" + String(pageCount));
-		    pageCount = pageCount + 1;
-		    var userPage = $("<div></div>");
+		function processPage(data){
+			var userPage = $("<div/>");
 			userPage.html(data.contents);
 			userTable = $('table[class="stripe"]', userPage);
-		    if ($('td[class="tc3_5"]', userTable) != null) { //check if there are still more entries
+			if ($('td[class="tc3_5"]', userTable) != null) { //check if there are still more entries
 				$('td[class="tc3_5"]:has(a)', userTable).each(function(i, obj){
 					userTitles.push($(this).text());
 				});
-		    } else {
-		    	lastPage = true;
-		    }		    
-		});	
-		
+		    }
+		}
 
-		$.getJSON('https://allorigins.me/get?url=' + encodeURIComponent($('#target').val()) + '&callback=?', function(data){
-		    var allTables = $("<div></div>");
+		for (let pageCount = 1; pageCount < 10; pageCount++) {
+			$.getJSON('https://allorigins.me/get?url=' + encodeURIComponent($('#list').val() + "?c=all;v=0;t=-1;o=a;s=title;p=" + String(pageCount)) + '&callback=?').done(function(data){
+			    processPage(data);
+			});
+		}
+
+		$.getJSON('https://allorigins.me/get?url=' + encodeURIComponent($('#target').val()) + '&callback=?').done(function(data){
+			console.log(userTitles);
+		    var allTables = $("<div/>");
 		    allTables.html(data.contents);
 			charTable = $('table[class="stripe"]', allTables).last();
 			$('td[class="tc1"]:has(a)', charTable).each(function(i, obj){
 				if (userTitles.includes($(this).text())) {
+					console.log($(this).text());
 					$('#results > tbody:last-child').append("<tr><td>" + $(this).html().replace("/", "https://vndb.org/") + "</td><td>" + $('td[class="tc3"]:has(a)', charTable).eq(i).html().replace("/", "https://vndb.org/") + "</td></tr>");
 				}
 			});
